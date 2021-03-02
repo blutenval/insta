9599cc026c98f44b0739f6436cddeb01533f7281
blutenval <79841204+blutenval@users.noreply.github.com>
2021-03-02 16:21:39 +0300

    Add files via upload

---
+++ Nekrasova Insta/Nekrasova Insta/Nekrasova_InstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nekrasova_InstaApp.swift
+//  Nekrasova Insta
+//
+//  Created by user on 02.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Nekrasova_InstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootContentView()
+        }
+    }
+}
